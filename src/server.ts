54685b4a71ffcb128324df3712722531a139d32d
juanlramos <juanl.bnr@gmail.com>
2026-01-28 18:23:38 -0300

    feat: Create the endpoint that search recomandations with AI (closes #1)

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { router } from "./routes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(";") || [],
+  }),
+);
 app.use(express.json());
+
+app.use(router);
 
 app.get("/", (req, res) => {
   res.send("Backend running!");
